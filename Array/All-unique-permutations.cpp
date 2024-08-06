45b210fec7513fba0239741de33cb9fee3b5b887
Wasiqah Neyaz <135358544+wasi-neyaz@users.noreply.github.com>
2024-08-06 23:22:23 +0530

    All-unique-permutations

---
+++ Array/All-unique-permutations.cpp
@@ -0,0 +1,15 @@
+
+
+class Solution {
+  public:
+    vector<vector<int>> uniquePerms(vector<int> &arr ,int n) {
+        // code here
+       sort(arr.begin(),arr.end());
+       vector<vector<int>>ans;
+       ans.push_back(arr);
+       while(next_permutation(arr.begin(),arr.end())){
+           ans.push_back(arr);
+       }
+       return ans;
+    }
+};

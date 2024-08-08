7e46fd91721357563f41aa59d8fcf48d08accd86
Wasiqah Neyaz <135358544+wasi-neyaz@users.noreply.github.com>
2024-08-08 23:55:47 +0530

    Palindrom-Linked-List

---
+++ LinkedList/Palindrom-Linked-List.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+ListNode* reverse(ListNode* head){
+    ListNode* prev = NULL;
+    ListNode* curr = head;
+    ListNode* next = curr->next;
+    
+    while(curr != NULL){
+        next = curr -> next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev; 
+}
+    bool isPalindrome(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* rev = reverse(slow);
+        while (rev != nullptr) {
+            if (head->val != rev->val) {
+                return false;
+            }
+            head = head->next;
+            rev = rev->next;
+        }
+        return true;
+        
+    }
+};

f2182851c16bd7791959ad111468a248b8966e3e
Wasiqah Neyaz <135358544+wasi-neyaz@users.noreply.github.com>
2024-08-08 23:57:33 +0530

    Remove-Duplicates-from-sorted-Linked-List

---
+++ LinkedList/Remove-Duplicates-from-sorted-Linked-List.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head == NULL || head-> next == NULL ){
+            return head;
+        }
+        ListNode* curr = head;
+        while(curr->next != NULL ){
+            if(curr->val == curr-> next -> val){
+                curr->next = curr-> next->next;
+            }
+            else{
+                curr = curr-> next ;
+            }
+        }
+        return head;
+        
+    }
+};
